224c6913cf61e8d191745a9ac6584daf77e4fd6a
albertcarrillosorolla <albertcarrillosorolla@gmail.com>
2015-09-08 10:25:26 +0200

    test2

--- ninus-scratch.js
+++ ninus-scratch.js
@@ -1,6 +1,4 @@
 (function(ext) {
-    var alarm_went_off = false; // This becomes true after the alarm goes off
-
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -10,31 +8,18 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.set_alarm = function(time) {
-       window.setTimeout(function() {
-           alarm_went_off = true;
-       }, time*1000);
-    };
-
-    ext.when_alarm = function() {
-       // Reset alarm_went_off if it is true, and return true
-       // otherwise, return false.
-       if (alarm_went_off === true) {
-           alarm_went_off = false;
-           return true;
-       }
-
-       return false;
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-            ['h', 'when alarm goes off', 'when_alarm'],
+            // Block type, block name, function name, param1 default value, param2 default value
+            ['r', '%n ^ %n', 'power', 2, 3],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Alarm extension', descriptor, ext);
+    ScratchExtensions.register('Sample extension', descriptor, ext);
 })({});
