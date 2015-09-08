a3255d350974defadcc522f5a111080ed4612786
albertcarrillosorolla <albertcarrillosorolla@gmail.com>
2015-09-08 15:00:20 +0200

    Update ninus-scratch.js

--- ninus-scratch.js
+++ ninus-scratch.js
@@ -1,4 +1,32 @@
 (function(ext) {
+
+    var usersData = [
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	},
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	},
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	},
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	},
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	},
+    	{id:-1, isTracked: false, position:[-100, -100], 
+    		interactions: {right-hand: false, left-hand: false, jumping: false},
+    		effectors: {right-hand:[0,0,0], left-hand:[0,0,0], right-foot:[0,0,0], left-foot:[0,0,0]}
+    	}];
+	
+	
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -12,10 +40,14 @@
     };
 
     ext.isTracked = function(user) {
-    	return false;
+    	return usersData[user].isTracked;
     };
     ext.getPosition = function(user, coordinate) {
-    	return 10;
+    	if(coordinate=="horizontal")
+    		return usersData[user].position[0];
+    	else if(coordinate == "vertical")
+    		return usersData[user].position[1];
+    	return null;
     };
     ext.getInteraction = function(user, interaction) {
     	return false;
@@ -43,4 +75,39 @@
 
     // Register the extension
     ScratchExtensions.register('Ninus', descriptor, ext);
+    
+    setInterval(function(){ 
+    	var xmlHttp = new XMLHttpRequest();
+    	xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    	xmlHttp.send( null );
+    	var users = xmlHttp.responseText.Split("#");
+    	for(int i=0; i<6; i++)
+    	{
+    		var data = users[i].Split('*');
+    		usersData[i].id = data[0].Split("/")[0];
+    		usersData[i].isTracked = data[0].Split("/")[1];
+    		
+    		usersData[i].position[0] = data[1].Split("/")[0];
+    		usersData[i].position[1] = data[1].Split("/")[1];
+    		
+    		usersData[i].interactions.right-hand= data[2].Split("/")[0];
+    		usersData[i].interactions.left-hand = data[2].Split("/")[1];
+    		usersData[i].interactions.jumping = data[2].Split("/")[2];
+    		
+    		usersData[i].effectors.rightHand[0] = data[3].Split("/")[0];
+    		usersData[i].effectors.rightHand[1] = data[3].Split("/")[1];
+    		usersData[i].effectors.rightHand[2] = data[3].Split("/")[2];
+    		usersData[i].effectors.leftHand[0] = data[3].Split("/")[3];
+    		usersData[i].effectors.leftHand[1] = data[3].Split("/")[4];
+    		usersData[i].effectors.leftHand[2] = data[3].Split("/")[5];
+    		usersData[i].effectors.rightFoot[0] = data[3].Split("/")[6];
+    		usersData[i].effectors.rightFoot[1] = data[3].Split("/")[7];
+    		usersData[i].effectors.rightFoot[2] = data[3].Split("/")[8];
+    		usersData[i].effectors.leftFoot[0] = data[3].Split("/")[9];
+    		usersData[i].effectors.leftFoot[1] = data[3].Split("/")[10];
+    		usersData[i].effectors.leftFoot[2] = data[3].Split("/")[11];
+    		
+    	}
+	}, 50);
+
 })({});
