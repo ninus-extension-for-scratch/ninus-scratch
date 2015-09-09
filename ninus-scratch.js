c1584bfd2808928b01003bf4b40adc4222d2f82c
albertcarrillosorolla <albertcarrillosorolla@gmail.com>
2015-09-09 11:16:01 +0200

    Update ninus-scratch.js

--- ninus-scratch.js
+++ ninus-scratch.js
@@ -65,7 +65,6 @@
     	var xmlHttp = new XMLHttpRequest();
     	xmlHttp.open( "GET", "http://127.0.0.1:14303/poll", false ); 
     	xmlHttp.send( null );
-    	var users = xmlHttp.responseText.Split("#");
     	
     };
 
