5e08f0532f3ffd45c741aea198a24cf7b3d2d3a8
albertcarrillosorolla <albertcarrillosorolla@gmail.com>
2017-01-24 09:58:41 +0100

    Update ninus-scratch.js

--- ninus-scratch.js
+++ ninus-scratch.js
@@ -202,7 +202,7 @@
 		["b", "is user %m.users tracked", "isTracked"],
 		["r", "user %m.users position %m.coordinate", "getPosition"],
 		["b", "is user %m.users %m.interaction", "getInteraction"],
-		["r", "user %m.users , effector %m.endeffector , coordinate %m.effectoraxis ", "getEffectorCoordiate"],
+		["r", "user %m.users , effector %m.endeffector , coordinate %m.effectoraxis", "getEffectorCoordiate"],
 		[" ", "Start vocabulary", "startVocabulary"],
 		[" ", "End vocabulary", "endVocabulary"],
 		[" ", "Add word %s", "addWord"],
